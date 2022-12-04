7ada193aaa97c1f3d0b6c5f3a39a36603f924681
Harlok <holmik90@yandex.ru>
2022-12-04 21:39:17 +0300

    update

--- stepik.org/Добрый, добрый Python. Сергей Балакирев/9. Генераторы. Некоторые полезные функции/9.1 Выражения генераторы/9.1.5 /9.1.5 Выражения генераторы.py
+++ stepik.org/Добрый, добрый Python. Сергей Балакирев/9. Генераторы. Некоторые полезные функции/9.1 Выражения генераторы/9.1.5 /9.1.5 Выражения генераторы.py
@@ -1,3 +1,8 @@
 from string import ascii_lowercase as asc
+"""
+Создает генератор, в котором хранятся строки 
+aa ab ac и т.д.
+"""
 gen = (i + j for i in asc for j in asc)
+"Выводим на экран первые 50 строк"
 print(*(next(gen) for i in range(50)))
