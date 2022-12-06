c0205d293690c9f23d87bec9c4678ac6e224e083
Harlok <holmik90@yandex.ru>
2022-12-07 00:12:52 +0300

    update

--- main_tests.py
+++ main_tests.py
@@ -1,7 +1,1 @@
-fields = ('id', 'title', 'pages')
-lst_value = [10, 'Название', 512]
-if len(lst_value) == 3:
-    z = zip(fields, lst_value)
 
-for k, v in z:
-    print(k, v)
--- stepik.org/ООП Сергей Балакирев/Первые шаги в ООП/1.4 Методы классов. Параметр self/1.4.3/1.4.3.py
+++ stepik.org/ООП Сергей Балакирев/Первые шаги в ООП/1.4 Методы классов. Параметр self/1.4.3/1.4.3.py
@@ -27,3 +27,14 @@ class StreamReader:
 
 sr = StreamReader()
 data, result = sr.readlines()
+
+
+"""
+fields = ('id', 'title', 'pages')
+lst_value = [10, 'Название', 512]
+if len(lst_value) == 3:
+    z = zip(fields, lst_value)
+
+for k, v in z:
+    print(k, v)
+"""
