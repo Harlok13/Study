15228bbb649e63363ac06eda6da225904ebbd393
Harlok <holmik90@yandex.ru>
2022-12-06 15:02:34 +0300

    update

--- main_tests.py
+++ main_tests.py
@@ -1,7 +1,8 @@
-lst_in = ['зонт=1000', 'палатка=10000', 'спички=22', 'котелок=543']
+class Notes:
+    uid = 1005435
+    title = 'Шутка'
+    author = 'И.С. Бах'
+    pages = 2
 
-lst = [tuple(i.split('=')) for i in lst_in]  # создаем список кортежей
 
-result = filter(lambda x: x if int(x[1]) >= 500 else None, lst)  # фильтруем по весу
-
-print(*(i[0] for i in result))  # выбираем только названия
+print(getattr(Notes, 'author', False))
