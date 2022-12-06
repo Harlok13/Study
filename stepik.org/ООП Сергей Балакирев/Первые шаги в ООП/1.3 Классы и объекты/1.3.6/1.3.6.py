6ac896d8404350ab61a6ca3d33beb226042e6639
Harlok <holmik90@yandex.ru>
2022-12-06 16:35:49 +0300

    update

--- stepik.org/ООП Сергей Балакирев/Первые шаги в ООП/1.3 Классы и объекты/1.3.6/1.3.6.py
+++ stepik.org/ООП Сергей Балакирев/Первые шаги в ООП/1.3 Классы и объекты/1.3.6/1.3.6.py
@@ -1,7 +1,32 @@
+"""
+Объявляем класс TravelBlog с атрибутом total_blogs, который
+будет увеличиваться с каждым новым созданным экземпляром класса
 
+Создаем 2 новых экземпляра класса
+"""
 class TravelBlog:
     total_blogs = 0
 
+    @classmethod
+    def count_total_blogs(cls):
+        cls.total_blogs += 1
+
+    def __init__(self, name, days):
+        self.name = name
+        self.days = days
+        self.count_total_blogs()
+
+    def __str__(self):
+        return self.name
+
+
+tb1 = TravelBlog('Франция', 6)
+tb2 = TravelBlog('Италия', 5)
+
+print(TravelBlog.__dict__)
+
+"""
+В случае, если не объявлять __init__ и декоратор classmethod
 
 tb1 = TravelBlog()
 setattr(tb1, 'name', 'Франция')
@@ -14,3 +39,4 @@ tb2.days = 5
 TravelBlog.total_blogs += 1
 
 print(TravelBlog.__dict__)
+"""
