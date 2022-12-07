44e696a3158847cb3d02259b795accb1a31666cc
Harlok <holmik90@yandex.ru>
2022-12-07 11:09:04 +0300

    update

--- main_tests.py
+++ main_tests.py
@@ -1,1 +1,12 @@
+lst_data = []
+lst_in = ['1 Сергей 35 120000', '2 Федор 23 12000', '3 Иван 13 1200']
+FIELDS = ('id', 'name', 'old', 'salary')
 
+
+lst = [i.split() for i in lst_in] # [['1', 'Сергей', '35', '120000'], ['2', 'Федор', '23', '12000'], ['3', 'Иван', '13', '1200']]
+
+for i in lst:
+    z = dict(zip(FIELDS, i))
+    lst_data.append(z)
+
+print(lst_data)
---
+++ stepik.org/ООП Сергей Балакирев/Первые шаги в ООП/1.4 Методы классов. Параметр self/1.4.4/1.4.4.py
@@ -0,0 +1,52 @@
+"""
+Решено самостоятельно
+"""
+
+lst_in = ['1 Сергей 35 120000', '2 Федор 23 12000', '3 Иван 13 1200']
+
+class DataBase:
+    """База данных"""
+    lst_data = []
+    FIELDS = ('id', 'name', 'old', 'salary')
+
+    @classmethod
+    def lst_data_update(cls, data):
+        """Преобразует переданный список значений в словари"""
+        lst = [i.split() for i in data]
+        cls.lst_data += [dict(zip(cls.FIELDS, i)) for i in lst]
+
+    def select(self, a, b):
+        """Возвращает выбранный диапазон"""
+        return self.lst_data[a:b + 1]
+
+    def insert(self, data):
+        """Добавляет в lst_data новый словарь"""
+        return self.lst_data_update(data)
+
+    # здесь добавлять методы
+
+
+db = DataBase()
+db.insert(lst_in)
+
+print(DataBase.__dict__)
+print(DataBase.lst_data)
+print(db.select(0, 3))
+
+
+"""
+Доп материал, использованный при решении
+
+lst_data = []
+lst_in = ['1 Сергей 35 120000', '2 Федор 23 12000', '3 Иван 13 1200']
+FIELDS = ('id', 'name', 'old', 'salary')
+
+
+lst = [i.split() for i in lst_in] # [['1', 'Сергей', '35', '120000'], ['2', 'Федор', '23', '12000'], ['3', 'Иван', '13', '1200']]
+
+for i in lst:
+    z = dict(zip(FIELDS, i))
+    lst_data.append(z)
+
+print(lst_data)
+"""
