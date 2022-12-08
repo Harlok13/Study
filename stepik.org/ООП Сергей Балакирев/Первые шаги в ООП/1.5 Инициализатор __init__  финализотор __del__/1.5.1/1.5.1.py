0ec2ae31b2a1c339649aaae865232c6fac3d46e3
Harlok <holmik90@yandex.ru>
2022-12-08 10:51:09 +0300

    update

---
+++ stepik.org/ООП Сергей Балакирев/Первые шаги в ООП/1.5 Инициализатор __init__  финализотор __del__/1.5.1/1.5.1.py
@@ -0,0 +1,10 @@
+"""Инициализируем экземпляр"""
+
+class Money:
+
+    def __init__(self, money):
+        self.money = money
+
+
+my_money = Money(100)
+your_money = Money(1000)
---
+++ stepik.org/ООП Сергей Балакирев/Первые шаги в ООП/1.5 Инициализатор __init__  финализотор __del__/1.5.2/1.5.2.py
@@ -0,0 +1,34 @@
+"""
+Решено самостоятельно
+
+Создаем список из 1000 экземпляров класса
+Меняем цвет 2го экземпляра на желтый
+"""
+
+class Point:
+
+    def __init__(self, x, y, color='black'):
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def __str__(self):
+        return self.color
+
+
+points = [Point(i, i) for i in range(1, 2001, 2)]
+points[1].color = 'yellow'
+
+print(len(points), points[1], sep='\n')
+
+print(points[1].__dict__)
+
+
+
+
+
+
+p1 = Point(10, 20)
+p2 = Point(12, 5, 'red')
+
+print(p1.__dict__, p2.__dict__, sep='\n')
