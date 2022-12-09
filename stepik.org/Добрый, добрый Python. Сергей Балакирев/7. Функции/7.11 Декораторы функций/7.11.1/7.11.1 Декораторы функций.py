d3edb49e0da342a83054283652f054c411d98836
Harlok <holmik90@yandex.ru>
2022-12-09 14:26:37 +0300

    update

--- stepik.org/Добрый, добрый Python. Сергей Балакирев/7. Функции/7.11 Декораторы функций/7.11.1/7.11.1 Декораторы функций.py
+++ stepik.org/Добрый, добрый Python. Сергей Балакирев/7. Функции/7.11 Декораторы функций/7.11.1/7.11.1 Декораторы функций.py
@@ -1,14 +1,13 @@
 """
+Вычисление площади прямоугольника и отображение
+с помощью декоратора
 Без проверки на ввод (числа)
-
 """
 
 def func_show(func):
     """Декоратор отображения результата вычисления"""
     def wrapper(*args, **kwargs):
-        res = func(*args, **kwargs)
-        print(f'Площадь прямоугольника: {res}')
-        return res
+        return print(f'Площадь прямоугольника: {func(*args, **kwargs)}')
 
     return wrapper
 
