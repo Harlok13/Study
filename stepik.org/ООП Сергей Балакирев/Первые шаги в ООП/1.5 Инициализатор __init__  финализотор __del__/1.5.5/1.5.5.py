8831b7548c0adda6f092df807eaa40071d1b7dde
Harlok <holmik90@yandex.ru>
2022-12-09 11:12:28 +0300

    update

---
+++ stepik.org/ООП Сергей Балакирев/Первые шаги в ООП/1.5 Инициализатор __init__  финализотор __del__/1.5.5/1.5.5.py
@@ -0,0 +1,55 @@
+"""
+Решено самостоятельно
+
+"""
+
+class Graph:
+
+    def __init__(self, data):
+        self.data = data.copy()  # важно делать копию, если не хотим изменений списка извне
+        self.is_show = True
+
+
+    def set_data(self, data):
+        """Передача нового списка данных"""
+        self.data = data
+
+
+    def show_table(self):
+        """Отображение данных в виде строки"""
+        print(*self.data if self.is_show else 'Отображение данных закрыто')
+
+    def show_graph(self):
+        """Отображение данных в виде графика"""
+        print('Графическое отображение данных:', *self.data if self.is_show else 'Отображение данных закрыто')
+
+
+    def show_bar(self):
+        """Отображение данных в виде столбчатой диаграммы"""
+        print('Столбчатая диаграмма:', *self.data if self.is_show else 'Отображение данных закрыто')
+
+    def set_show(self, fl_show):
+        """Изменение локального свойства"""
+        self.is_show = fl_show
+
+
+
+
+
+
+data_graph = [8, 11, 10, -32, 0, 7, 18]
+gr = Graph(data_graph)
+gr1 = Graph(data_graph)
+gr.show_bar()
+gr.show_table()
+# gr.set_show(False)
+gr.show_graph()
+gr.set_data([3, 6, 18, -35, 1, 21, 4])
+gr.show_graph()
+gr1.show_bar()
+gr2 = Graph(data_graph)
+gr2.show_bar()
+
+print(id(gr.data))
+print(id(gr1.data))
+print(id(gr2.data))
