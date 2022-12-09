2998b1ce876520d32b13b738b53acc623a6d2fac
Harlok <holmik90@yandex.ru>
2022-12-09 15:49:38 +0300

    update

---
+++ stepik.org/Добрый, добрый Python. Сергей Балакирев/7. Функции/7.11 Декораторы функций/7.11.3/7.11.3 Декораторы функций.py
@@ -0,0 +1,15 @@
+def sorted_lst(func):
+    """Сортировка списка чисел"""
+    def wrapper(*args, **kwargs):
+        return sorted(func(*args, **kwargs))
+    return wrapper
+
+@sorted_lst
+def get_list(s):
+    """Преобразует строку в список целых чисел"""
+    return list(map(int, s.split()))
+
+
+print(*get_list(['8 11 -5 4 3 10']))
+
+
