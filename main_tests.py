7b550bfbf2a32ffdff7ae4cc4e1a37fafe8a4a9c
Harlok <holmik90@yandex.ru>
2022-12-10 10:22:32 +0300

    update

--- main_tests.py
+++ main_tests.py
@@ -1,1 +1,8 @@
-print(None==0)
+from random import randint as r
+a, b, c = 1, 2, 3
+lst = [1, 2, 3]
+ma = max(lst)
+mi = min(lst)
+print(set([f'{r(mi, ma)}{r(mi, ma)}{r(mi, ma)}' for i in range(333)]))
+
+
