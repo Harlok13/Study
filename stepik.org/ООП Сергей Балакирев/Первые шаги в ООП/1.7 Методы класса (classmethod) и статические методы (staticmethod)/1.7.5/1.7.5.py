cc5bc09365c91ae6d0c45be1c48f2caee7f8712b
Harlok <holmik90@yandex.ru>
2022-12-12 22:55:33 +0300

    update

--- stepik.org/ООП Сергей Балакирев/Первые шаги в ООП/1.7 Методы класса (classmethod) и статические методы (staticmethod)/1.7.5/1.7.5.py
+++ stepik.org/ООП Сергей Балакирев/Первые шаги в ООП/1.7 Методы класса (classmethod) и статические методы (staticmethod)/1.7.5/1.7.5.py
@@ -1,1 +1,17 @@
-class
+class AppStore:
+    def add_application(self, app):
+        pass
+
+    def remove_application(self, app):
+        pass
+
+    def block_application(self, app):
+        pass
+
+    def total_apps(self):
+        pass
+
+
+class Application:
+    def __init__(self, name, blocked):
+        pass
