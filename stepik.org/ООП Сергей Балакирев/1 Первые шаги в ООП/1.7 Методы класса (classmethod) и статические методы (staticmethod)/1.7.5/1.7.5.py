f6bd6f40e29004410bc5a7100d87d3456c67313c
Harlok <holmik90@yandex.ru>
2022-12-13 11:14:45 +0300

    update

--- stepik.org/ООП Сергей Балакирев/1 Первые шаги в ООП/1.7 Методы класса (classmethod) и статические методы (staticmethod)/1.7.5/1.7.5.py
+++ stepik.org/ООП Сергей Балакирев/1 Первые шаги в ООП/1.7 Методы класса (classmethod) и статические методы (staticmethod)/1.7.5/1.7.5.py
@@ -1,17 +1,42 @@
-class AppStore:
-    def add_application(self, app):
-        pass
-
-    def remove_application(self, app):
-        pass
-
-    def block_application(self, app):
-        pass
-
-    def total_apps(self):
-        pass
+"""
+Решено самостоятельно
+"""
 
 
 class Application:
-    def __init__(self, name, blocked):
-        pass
+    def __init__(self, name: str, blocked=False):
+        self.name = name
+        self.blocked = blocked
+
+
+class AppStore:
+    __instance = None
+    apps = []
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            return super().__new__(cls)
+        return cls.__instance
+
+    @classmethod
+    def add_application(cls, app: Application):
+        cls.apps += [app]
+
+    @classmethod
+    def remove_application(cls, app: Application):
+        cls.apps.remove(app)
+
+    @staticmethod
+    def block_application(app: Application):
+        app.blocked = True
+
+    @classmethod
+    def total_apps(cls) -> int:
+        return len(cls.apps)
+
+
+store = AppStore()
+app_youtube = Application("Youtube")
+store.add_application(app_youtube)
+print(store.apps)
+# store.remove_application(app_youtube)
