756281643c8f8d29c9aa1eb833dca5b401bbeb39
Harlok <holmik90@yandex.ru>
2022-12-14 17:49:08 +0300

    update

---
+++ stepik.org/ООП Сергей Балакирев/2 Режимы доступа, свойства и дескрипторы/2.1 Режимы доступа public, private, protected. Сеттеры и геттеры/2.1.3/2.1.3.py
@@ -0,0 +1,28 @@
+"""
+Ничего интересного, просто дублирование кода согласно ТЗ
+"""
+
+
+class Book:
+    def __init__(self, author, title, price):
+        self.__author = author
+        self.__title = title
+        self.__price = price
+
+    def set_title(self, title):
+        self.__title = title
+
+    def set_author(self, author):
+        self.__author = author
+
+    def set_price(self, price):
+        self.__price = price
+
+    def get_title(self):
+        return self.__title
+
+    def get_author(self):
+        return self.__author
+
+    def get_price(self):
+        return self.__price
