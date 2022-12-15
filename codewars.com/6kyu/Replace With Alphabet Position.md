b6bf1591595fca55b236e646591f662447e5a0de
Harlok <holmik90@yandex.ru>
2022-12-15 11:04:27 +0300

    update

---
+++ codewars.com/6kyu/Replace With Alphabet Position.md
@@ -0,0 +1,30 @@
+**DESCRIPTION:**
+
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+**Example**
+
+alphabet_position("The sunset sets at twelve o' clock.")\
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+# Solution
+
+```
+from string import ascii_lowercase
+def alphabet_position(text):
+    d = {v: k for k, v in enumerate(ascii_lowercase, 1)}
+    return ' '.join(map(str, [d.get(i, '') for i in text.lower() if i in d]))
+```
+___
+# Other Solutions
+
+```
+def alphabet_position(text):
+    return ' '.join(str(ord(c) - 96) for c in text.lower() if c.isalpha())
+```
--- main_tests.py
+++ main_tests.py
@@ -1,45 +1,4 @@
-# def power_sumDigTerm(n):
-#
-#     gen = (i for i in range(1_000_000) if sum(map(int, list(str(i)))) ** len(list(str(i))) == i and i > 9)
-#     lst_of_gen = [next(gen) for _ in range(n)]
-#     return lst_of_gen
-#
-#
-# print(power_sumDigTerm(3))
-# from math import sqrt
-
-
-# def power_sumDigTerm(n):
-#     for i in range(1_000_000):
-#         if sum(map(int, list(str(i)))) ** len(list(str(i))) == i and i > 9:
-#             yield i
-#
-#
-# # print(power_sumDigTerm(2))
-#
-# for i in power_sumDigTerm(1):
-#     print(i)
-
-
-# def power_sumDigTerm(n):
-#     lst = list(map(int, list(str(n))))  # преобразует число в список цифр, потом преобразует эти цифры в инт
-#     s = sum(lst)  # складывает эти числа
-#     res = s ** len(lst)
-#     return res == n
-#
-# print(power_sumDigTerm(4913))
-#
-# print(18**3)
-
-def power_sumDigTerm(n):
-    if n == 1:
-        return 81
-    elif n ==2:
-        return 512
-    elif n == 3:
-        return 2401
-    gen = (i for i in range(1_000_000) if sum(map(int, list(str(i)))) ** 3 == i and i > 9)
-    lst_of_gen = [next(gen, False) for _ in range(n)]
-    return lst_of_gen
-
-print(power_sumDigTerm(4))
+from string import ascii_lowercase
+d = {v: k for k, v in enumerate(ascii_lowercase, 1)}
+print(d)
+print(' '.join(map(str, [d.get(i, '') for i in "The sunset sets at twelve o' clock.".lower() if i in d])))
