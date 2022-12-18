ee5a6c0954cf6b612008c5046d1fad41aca8df4d
Harlok <holmik90@yandex.ru>
2022-12-18 16:49:07 +0300

    update

--- main_tests.py
+++ main_tests.py
@@ -1,38 +1,22 @@
-# # a = [1,2],[6, 10],[11, 15]
-# r = [set(range(1, 4)), set(range(7, 10)), set(range(3, 5))]
-# # a = set(range(1, 2))
-# # b = set(range(6, 10))
-# # c = set(range(11,15))
-# # print(len(a) + len(b) + len(c))
-#
-#
-# # k = set()
-# # for i in r:
-# #     k = k.union(i)
-# # print(k)
-#
-# inter = [[1, 2], [6, 10], [11, 15]]
-#
-#
-# def sum_of_intervals(intervals):
-#
-#     # lst = list(map(lambda a,b: set(range(a,b)), intervals))
-#     lst = [set(range(k, j)) for k, j in intervals]
-#     s = set()
-#     for i in lst:
-#         s = s.union(i)
-#     print(s)
-#     return len(s)
-#
+class MyClass:
+    def __init__(self, var):
+        self.var = var
 
-def sum_of_intervals(intervals):
-    s, top = 0, float("-inf")
-    for a,b in sorted(intervals):
-        if top < a: top    = a
-        if top < b: s, top = s+b-top, b
-    return s
+    @property
+    def var(self):
+        return self.__var
 
-inter = [[8,9], [1, 2], [6, 10], [11, 15], [4,12], [21, 25], [15, 19], [7, 26]]
-print(sum_of_intervals(inter))
-print(float('-inf'))
-print(sorted(inter))
+    @var.setter
+    def var(self, value):
+        if self.__validate(value):
+            self.__var = value
+
+
+    @staticmethod
+    def __validate(value):
+        return type(value) == int
+
+
+
+c = MyClass('10')
+print(c.__dict__)
--- stepik.org/ООП Сергей Балакирев/2 Режимы доступа, свойства и дескрипторы/2.2 Свойства property. Декоратор @property/2.2.5/2.2.5.py
+++ stepik.org/ООП Сергей Балакирев/2 Режимы доступа, свойства и дескрипторы/2.2 Свойства property. Декоратор @property/2.2.5/2.2.5.py
@@ -1,32 +1,71 @@
 class TreeObj:
     def __init__(self, indx, value=None):
-        self.indx = indx
+        self.index = indx
         self.value = value
-        self.__left = None
-        self.__right = None
+        self.left = self.right = None
 
     @property
     def left(self):
         return self.__left
 
     @left.setter
-    def left(self, value):
-        self.left = value
+    def left(self, obj):
+        self.__left = obj
 
     @property
     def right(self):
         return self.__right
 
     @right.setter
-    def right(self, value):
-        self.__right = value
+    def right(self, obj):
+        self.__right = obj
 
 
 class DecisionTree:
+    root = None
+
     @classmethod
-    def predict(cls, root: 'DecisionTree', x):
-        pass
+    def predict(cls, root: 'DecisionTree', x: list[int]):
+        obj = root
+        while obj:
+            obj_next = cls.get_next(obj, x)
+            if obj_next is None:
+                break
+            obj = obj_next
+        return obj.value
 
     @classmethod
     def add_obj(cls, obj: TreeObj, node: 'DecisionTree' = None, left=True):
-        pass
+        """
+
+        :param obj: reference to the newly added object
+        :param node: a reference to the tree object to which node obj is attached
+        :param left: flag that defines the branch of the tree to which the object is attached
+        :return:
+        """
+        if node:
+            if left:
+                node.left = obj
+            else:
+                node.right = obj
+
+        return obj
+
+    @classmethod
+    def get_next(cls, obj, x):
+        if x[obj.index] == 1:
+            return obj.left
+        return obj.right
+
+
+root = DecisionTree.add_obj(TreeObj(0))
+v_11 = DecisionTree.add_obj(TreeObj(1), root)
+v_12 = DecisionTree.add_obj(TreeObj(2), root, False)
+DecisionTree.add_obj(TreeObj(-1, "будет программистом"), v_11)
+DecisionTree.add_obj(TreeObj(-1, "будет кодером"), v_11, False)
+DecisionTree.add_obj(TreeObj(-1, "не все потеряно"), v_12)
+DecisionTree.add_obj(TreeObj(-1, "безнадежен"), v_12, False)
+
+x = [1, 1, 0]
+res = DecisionTree.predict(root, x)
+print(res)
