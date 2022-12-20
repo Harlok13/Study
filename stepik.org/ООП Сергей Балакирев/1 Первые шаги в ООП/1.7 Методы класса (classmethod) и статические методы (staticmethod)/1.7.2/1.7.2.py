b49cafdc95b3498209c58c4efdb8d2dc460dd3e3
Harlok <holmik90@yandex.ru>
2022-12-20 14:12:31 +0300

    update

--- stepik.org/ООП Сергей Балакирев/1 Первые шаги в ООП/1.7 Методы класса (classmethod) и статические методы (staticmethod)/1.7.2/1.7.2.py
+++ stepik.org/ООП Сергей Балакирев/1 Первые шаги в ООП/1.7 Методы класса (classmethod) и статические методы (staticmethod)/1.7.2/1.7.2.py
@@ -1,15 +1,39 @@
-from string import ascii_lowercase, digits
+"""
+classmethod не особо и нужен с таким решением
+"""
+from re import fullmatch
+
 
 class TextInput:
-    def __init__(self, name, size):
-        self.name = name
+    def __init__(self, name, size=10):
         self.size = size
+        if self.check_name(name):
+            self.name = name
+        else:
+            raise ValueError("некорректное поле name")
+
+    def get_html(self):
+        return f"<p class='login'>{self.name}: <input type='text' size={self.size} />"
+
+    @classmethod
+    def check_name(cls, name):
+        return 50 >= len(name) >= 3 and fullmatch(r'^[a-zA-Z0-9А-Яа-яЁё ]{3,}$', name)
 
 
 class PasswordInput:
-    def __init__(self, name, size):
-        self.name = name
+    def __init__(self, name, size=10):
+        self.name = None
         self.size = size
+        if self.check_name(name):
+            self.name = name
+
+    def get_html(self):
+        return f"<p class='password'>{self.name}: <input type='text' size={self.size} />"
+
+    @classmethod
+    def check_name(cls, name):
+        if 50 >= len(name) >= 3 and fullmatch(r'^[a-zA-Z0-9А-Яа-яЁё ]{3,}$', name):
+            return True
 
 
 class FormLogin:
@@ -24,3 +48,4 @@ class FormLogin:
 # эти строчки не менять
 login = FormLogin(TextInput("Логин"), PasswordInput("Пароль"))
 html = login.render_template()
+print(html)
