71327d22c4319602c397d315b30b6cda2dbe9c98
Harlok <holmik90@yandex.ru>
2022-12-21 15:36:39 +0300

    update

--- stepik.org/ООП Сергей Балакирев/2 Режимы доступа, свойства и дескрипторы/2.2 Свойства property. Декоратор @property/2.2.6/2.2.6.test.py
+++ stepik.org/ООП Сергей Балакирев/2 Режимы доступа, свойства и дескрипторы/2.2 Свойства property. Декоратор @property/2.2.6/2.2.6.test.py
@@ -1,38 +1,47 @@
+from math import sqrt
+
+
 class PathLines:
     def __init__(self, *args):
         self.start_x = self.start_y = 0
         self.lst = [*args]
 
 
-class LineTo:
-    lst = []
+    def get_path(self):
+        return self.lst
 
-    def __new__(cls, *args, **kwargs):
-        cls.lst += [super().__new__(cls)]
-        return super().__new__(cls)
+
+    def get_length(self):
+        l = len(self.lst) + 1
+        lst = self.lst
+        L = 0
+        for i in range(l):
+            if i != l:
+                L += sqrt((pow(lst[i][0] - lst[i+1][0], 2) + pow(lst[i][1] - lst[i+1][1], 2)))
+        return L
+
+    @classmethod
+    def add_line(self, line):
+        self.lst.append(line)
+
+class LineTo:
+
+    # def __new__(cls, *args, **kwargs):
+    #     cls.lst += [super().__new__(cls)]
+    #     return super().__new__(cls)
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def __repr__(self):
-        return f'({self.x}, {self.y})'
 
-    @classmethod
-    def get_path(cls):
-        return cls.lst
 
-    @staticmethod
-    def get_length():
-        pass
 
-    def add_line(self, line):
-        pass
 
 
 line1 = LineTo(10, 20)
 line2 = LineTo(15, 25)
 line3 = LineTo(20, 30)
 p = PathLines(line1, line2, line3)
-print(p.lst)
-print(line1.get_path())
+print(p.get_length())
+print(p.get_path())
