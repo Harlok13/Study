9d3dd8ae3ee1670364328e2c857486b75de17b64
Harlok <holmik90@yandex.ru>
2022-12-22 23:53:26 +0300

    update

--- main_tests_2.py
+++ main_tests_2.py
@@ -1,27 +1,5 @@
-"""
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
+res = [float(i+1)*float(j+1) for i in range(5)
+       for j in range(3)
+]
 
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
-"""
-
-
-
-def two_sum(nums, target: int):
-    # res = [i for i in nums if i <= target]
-    d = {}
-    for k, v in enumerate(nums):
-
-        if not d:
-            d.update({abs(target - v): k})
-        else:
-            if v not in d:
-                d.update({target - v: k})
-            else:
-
-                return [d.get(v), k]
-
-
-
-print(two_sum([0,3,-3,4,-1], -1))
+print(res)
--- stepik.org/ООП Сергей Балакирев/2 Режимы доступа, свойства и дескрипторы/2.3 Дескрипоторы (data descriptor и non-data descriptor)/2.3.1/2.3.1.py
+++ stepik.org/ООП Сергей Балакирев/2 Режимы доступа, свойства и дескрипторы/2.3 Дескрипоторы (data descriptor и non-data descriptor)/2.3.1/2.3.1.py
@@ -3,12 +3,33 @@ class FloatValue:
         return getattr(instance, self._name)
 
     def __set__(self, instance, value):
-        setattr(instance, self._name, value)
+        if self.__check_value(value):
+            setattr(instance, self._name, value)
+
 
     def __set_name__(self, owner, name):
         self._name = '_' + name
 
+    @staticmethod
+    def __check_value(value):
+        if type(value) != float:
+            raise TypeError("Присваивать можно только вещественный тип данных.")
+        return True
+
+
 class Cell:
-    table = FloatValue()
-    def __init__(self):
-        self.table = ...
+    value = FloatValue()
+
+    def __init__(self, value=0.0):
+        self.value = value
+
+
+class TableSheet:
+    def __init__(self, N, M):
+        self.cells = [[Cell(float(x + 1) * float(i + 1)) for x in range(M)] for i in range(N)]
+
+
+
+
+table = TableSheet(5, 3)
+print(table.__dict__)
