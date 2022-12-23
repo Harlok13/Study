e95e9a2d293dc1716e28a6059769c1db9f1ccfb3
Harlok <holmik90@yandex.ru>
2022-12-23 21:02:42 +0300

    update

--- stepik.org/ООП Сергей Балакирев/2 Режимы доступа, свойства и дескрипторы/2.3 Дескрипоторы (data descriptor и non-data descriptor)/2.3.2/2.3.2.py
+++ stepik.org/ООП Сергей Балакирев/2 Режимы доступа, свойства и дескрипторы/2.3 Дескрипоторы (data descriptor и non-data descriptor)/2.3.2/2.3.2.py
@@ -1,1 +1,18 @@
+class ValidateString:
+    def __init__(self, min_length=3, max_length=100):
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def validate(self, string):
+        return isinstance(string, str) and self.min_length <= len(string) <= self.max_length
+
+
+class StringValue():
+    pass
+
+
+class RegisterForm:
+    logic = StringValue()
+    password = StringValue()
+    email = StringValue()
 
