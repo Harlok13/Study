e99573f3a2c40270a838a7bbfdd4d19b89cb2381
Harlok <holmik90@yandex.ru>
2023-01-06 20:42:36 +0300

    update

---
+++ stepik.org/ООП Сергей Балакирев/3 Магические методы классов/3.2 Метод __call__. Функторы и классы-декораторы/3.2.1/3.2.1.py
@@ -0,0 +1,7 @@
+class RandomPassword:
+    def __init__(self, psw_chars, min_length, max_length):
+        self.psw_chars = psw_chars
+        self.min_length = min_length
+        self.max_length = max_length
+
+
