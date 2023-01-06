adbf6555d77c2c215affa8b3f354f1efa559adfc
Harlok <holmik90@yandex.ru>
2023-01-06 21:26:18 +0300

    update

--- stepik.org/ООП Сергей Балакирев/3 Магические методы классов/3.2 Метод __call__. Функторы и классы-декораторы/3.2.1/3.2.1.py
+++ stepik.org/ООП Сергей Балакирев/3 Магические методы классов/3.2 Метод __call__. Функторы и классы-декораторы/3.2.1/3.2.1.py
@@ -1,7 +1,19 @@
+import random
+
+
 class RandomPassword:
     def __init__(self, psw_chars, min_length, max_length):
         self.psw_chars = psw_chars
         self.min_length = min_length
         self.max_length = max_length
 
+    def __call__(self, *args, **kwargs):
+        return ''.join(random.choices(self.psw_chars,
+                       k=random.randint(self.min_length, self.max_length)))
 
+
+rnd = RandomPassword("qwertyuiopasdfghjklzxcvbnm0123456789!@#$%&*", 5, 20)
+print(rnd())
+
+lst_pass = [rnd() for _ in range(3)]
+print(lst_pass)
