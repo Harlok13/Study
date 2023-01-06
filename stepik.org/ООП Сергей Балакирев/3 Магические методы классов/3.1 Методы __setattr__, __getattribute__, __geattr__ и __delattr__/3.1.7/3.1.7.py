d48d7d3290bd587dd5f374c879f112b217fc489b
Harlok <holmik90@yandex.ru>
2023-01-06 12:06:11 +0300

    update

---
+++ stepik.org/ООП Сергей Балакирев/3 Магические методы классов/3.1 Методы __setattr__, __getattribute__, __geattr__ и __delattr__/3.1.7/3.1.7.py
@@ -0,0 +1,47 @@
+class Dimensions:
+    MIN_DIMENSION = 10
+    MAX_DIMENSION = 1000
+
+    def __init__(self, a, b, c):
+        self.__a = a
+        self.__b = b
+        self.__c = c
+
+    @property
+    def a(self):
+        return self.__a
+
+    @a.setter
+    def a(self, value):
+        self.__a = value
+
+    @property
+    def b(self):
+        return self.__b
+
+    @b.setter
+    def b(self, value):
+        self.__b = value
+
+    @property
+    def c(self):
+        return self.__c
+
+    @c.setter
+    def c(self, value):
+        self.__c = value
+
+    def __setattr__(self, key, value):
+        if key in ('MIN_DIMENSION', 'MAX_DIMENSION'):
+            raise AttributeError("Менять атрибуты MIN_DIMENSION и MAX_DIMENSION запрещено.")
+        if type(value) in (int, float) and value in range(self.MIN_DIMENSION, self.MAX_DIMENSION + 1):
+            super().__setattr__(key, value)
+
+
+d = Dimensions(10.5, 20.1, 30)
+d.a = 8
+d.b = 15
+a, b, c = d.a, d.b, d.c  # a=10.5, b=15, c=30
+d.MAX_DIMENSION = 10  # исключение AttributeError
+
+
