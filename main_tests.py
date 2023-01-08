531023035169983ed1f62081cfc2da21cddd682d
Harlok <holmik90@yandex.ru>
2023-01-08 12:55:39 +0300

    update

--- main_tests.py
+++ main_tests.py
@@ -1,4 +1,6 @@
-x = 6
-y = 3
-x = x + y
-print(x)
+from collections import Counter
+def find_secret_message(p):
+    res = dict(Counter(p.lower().replace('.', '').split()))
+    return ' '.join([k for k, v in res.items() if v > 1])
+
+print(find_secret_message('This is a test. this test is fun.'))
