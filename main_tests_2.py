f34200da61b25b9a76408b0000a51e612e055218
Harlok <holmik90@yandex.ru>
2023-01-09 23:43:51 +0300

    update

--- main_tests_2.py
+++ main_tests_2.py
@@ -1,11 +1,64 @@
-def expanded_form(num):
-    lst = reversed(list(str(num)))
-    fil = filter(lambda x: x != '0', [str(int(v) * (1, 10 ** k)[k>0]) for k, v in enumerate(lst)])
-    return ' + '.join(reversed(list(fil)))
+operands = {'plus': '+', 'minus': '-', 'times': '*', 'divided_by': '/'}
+def zero(operand=None):
+    if operand:
+        expression = operands.get(operand.split()[0]) + operand.split()[1]
+        return eval(f'0 {expression}')
+    return 0
+def one(operand=None):
+    if operand:
+        expression = operands.get(operand.split()[0]) + operand.split()[1]
+        return eval(f'1 {expression}')
+    return 1
+def two(operand=None):
+    if operand:
+        expression = operands.get(operand.split()[0]) + operand.split()[1]
+        return eval(f'2 {expression}')
+    return 2
+def three(operand=None):
+    if operand:
+        expression = operands.get(operand.split()[0]) + operand.split()[1]
+        return eval(f'3 {expression}')
+    return 3
+
+def four(operand=None):
+    if operand:
+        expression = operands.get(operand.split()[0]) + operand.split()[1]
+        return eval(f'4 {expression}')
+    return 4
+def five(operand=None):
+    if operand:
+        expression = operands.get(operand.split()[0]) + operand.split()[1]
+        return eval(f'5 {expression}')
+    return 5
+def six(operand=None):
+    if operand:
+        expression = operands.get(operand.split()[0]) + operand.split()[1]
+        return eval(f'6 {expression}')
+    return 6
+def seven(operand=None):
+    if operand:
+        expression = operands.get(operand.split()[0]) + operand.split()[1]
+        return eval(f'7 {expression}')
+    return 7
+def eight(operand=None):
+    if operand:
+        expression = operands.get(operand.split()[0]) + operand.split()[1]
+        return eval(f'8 {expression}')
+    return 8
+def nine(operand=None):
+    if operand:
+        expression = operands.get(operand.split()[0]) + operand.split()[1]
+        return eval(f'9 {expression}')
+    return 9
+
+def plus(var): return f'plus {var}'
+def minus(var): return f'minus {var}'
+def times(var): return f'times {var}'
+def divided_by(var): return f'divided_by {var}'
 
 if __name__ == '__main__':
-    print(expanded_form(70304))
-
+    print(zero(plus(six())))
+    print(plus(six()))
 
 
 """
