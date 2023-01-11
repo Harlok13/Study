bae049e762a0cc7b4a9cc5b22903330019f8d348
Harlok <holmik90@yandex.ru>
2023-01-12 00:36:26 +0300

    update

---
+++ Librarys, modules/re/sub/sub_examples.py
@@ -0,0 +1,12 @@
+def split_string(sentence: str, *, sub: str = '.') -> list:
+    """Делит строку по любым символам, которые не являются буквами"""
+    pattern = ''.join((i for i in sentence if not i.isalpha()))
+    res_strng = __import__('re').sub(fr'[{pattern}]', fr'{sub}', sentence.strip(pattern))
+    return res_strng.split(sub)
+
+
+if __name__ == '__main__':
+    print(split_string('sdf,sdf;'))
+    assert split_string('sdf,sdf;') == ['sdf', 'sdf']
+    assert split_string('сколько.лет:сколько"зим') == ['сколько', 'лет', 'сколько', 'зим']
+    assert split_string('не;много:не№мало!', sub='#') == ['не', 'много', 'не', 'мало']
