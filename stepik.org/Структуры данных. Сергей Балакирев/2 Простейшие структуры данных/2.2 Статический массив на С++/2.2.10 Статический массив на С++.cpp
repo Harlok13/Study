63a983b15bd0d04f0daffc1359643dc91a5f868a
Harlok <holmik90@yandex.ru>
2023-01-25 23:14:35 +0300

    update

---
+++ stepik.org/Структуры данных. Сергей Балакирев/2 Простейшие структуры данных/2.2 Статический массив на С++/2.2.10 Статический массив на С++.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(void)
+{
+    using namespace std;
+    // здесь пишите программу
+
+    __assertion_tests(v1, v2, v_res);    // эту строчку не убирать (должна стоять перед return 0)
+    return 0;
+}
---
+++ stepik.org/Структуры данных. Сергей Балакирев/2 Простейшие структуры данных/2.2 Статический массив на С++/2.2.9 Статический массив на С++.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(void)
+{
+    using namespace std;
+    // здесь пишите программу
+
+    __assertion_tests(marks, m_avr);    // эту строчку не убирать (должна стоять перед return 0)
+    return 0;
+}
