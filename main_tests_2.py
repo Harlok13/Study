592c495ca2d6d7b32e8384c658c48b6375dbbb5b
Harlok <holmik90@yandex.ru>
2023-01-26 23:22:22 +0300

    update

--- main_tests_2.py
+++ main_tests_2.py
@@ -1,18 +1,8 @@
-def count_sequence(iter_obj: iter) -> int:
-    last_num, last_count, res_count = None, 1, 1
-    for i in iter_obj:
-        if i == last_num: last_count += 1
-        else: last_count = 1
-        if last_count > res_count: res_count = last_count
-        last_num = i
-    return res_count
+lst_in = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-
-tup1 = (1, 3, 2, 2, 4, 2, 2, 2, 2, 4, 6, 3, 2, 6, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 0)
-tup2 = (3, 3, 2, 2, 5, 3, 5, 5, 5, 5, 5, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0)
-if __name__ == '__main__':
-    assert count_sequence(tup1) == 8
-    assert count_sequence(tup2) == 9
-
+try:
+    while a := int(input()):
+        print(a)
+except EOFError: pass
 
 
