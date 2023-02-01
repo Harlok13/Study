3552e5a328edbb32def3702cfa9afa42bea853af
Harlok <holmik90@yandex.ru>
2023-02-01 13:35:37 +0300

    update

--- tests/division_test.py
+++ tests/division_test.py
@@ -2,5 +2,20 @@ from librarys_modules.pytest_.utils import division
 import pytest
 
 
-def test_division():
-    assert division(10, 2) == 5
+@pytest.mark.parametrize('a, b, expected_result', [(10, 2, 5),
+                                                   (20, 5, 4),
+                                                   (30, -3, -10),
+                                                   (5, 2, 2.5)])
+def test_division(a, b, expected_result):
+    """Проверка на корректные значения при делении"""
+    assert division(a, b) == expected_result
+
+
+@pytest.mark.parametrize('expected_exception, divisionable, divider',
+                         [(ZeroDivisionError, 10, 0),
+                          (TypeError, 20, '2')])
+def test_zero_division(divisionable, divider, expected_exception):
+    """Проверка на генерацию исключений"""
+    with pytest.raises(expected_exception):
+        division(divisionable, divider)
+
