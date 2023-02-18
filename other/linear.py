7cb1668ab2cd32597010265eac348fc24aea53da
Harlok <holmik90@yandex.ru>
2023-02-18 23:40:08 +0300

    upd

--- other/linear.py
+++ other/linear.py
@@ -1,11 +1,14 @@
+# import pytest
+
 collections_not_dict = [list, tuple]
 
 
 def flatten_dict(nested_lists):
     res = []
     for key, value in nested_lists.items():
-        if not isinstance(key, dict):
+        if key:
             res.append(key)
+
         if not isinstance(value, dict):
             if type(value) in collections_not_dict:
                 res += linear(value)
@@ -28,8 +31,14 @@ def linear(nested_lists):
     return res
 
 
-TEST_DATA = [1, 'abc', [[100, 200], 10, 20], [30, 40]]
-EXPECTED = [1, 'abc', 100, 200, 10, 20, 30, 40]
+TEST_DATA = ([1, 'abc', [[100, 200], 10, 20], [30, 40]],
+             [[1, '2', 3], (4, 5),  [6, {'7': {8: [9, 10]}}], 11, 12, [13, 14, 15], [16], 17, [[18], [19, 20, [[[21]]]], {22: (23, '24')}]]
+)
+EXPECTED = ([1, 'abc', 100, 200, 10, 20, 30, 40],
+            [1, '2', 3, 4, 5, 6, '7', 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, '24'])
+
 
 if __name__ == '__main__':
-    assert linear(TEST_DATA) == EXPECTED
+    for indx in range(2):
+        assert linear(TEST_DATA[indx]) == EXPECTED[indx], f'{linear(TEST_DATA[indx])}'
+
